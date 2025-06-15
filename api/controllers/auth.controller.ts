d158cd483817808b5bf2b1b4e7d636af5527a11e
sreekanthkotagiri <sreekotagiri4u@gmail.com>
2025-06-15 21:16:08 +0530

    Fix bcrypt import error

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { db } from '../index';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import PDFDocument from 'pdfkit';
 
