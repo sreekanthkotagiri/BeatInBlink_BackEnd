428bc38943291c9876f356216d9589507d54262e
sreekanthkotagiri <sreekotagiri4u@gmail.com>
2025-06-26 10:03:56 +0530

    Fix ArrayBuffer error in Express response

--- api/index.ts
+++ api/index.ts
@@ -82,7 +82,7 @@ db.connect()
 
 // Routes
 app.get('/', (_req: Request, res: Response) => {
-  res.send(JSON.stringify({ message: 'EduExamine API is running 🎓' }));
+  res.json({ message: 'EduExamine API is running 🎓' });
 });
 
 app.use('/api/protected', protectedRoutes);
