4880551ff424ccac84ef30df71dea81ca686a1ff
sreekanthkotagiri <sreekotagiri4u@gmail.com>
2025-07-11 12:07:17 +0530

    Fix ArrayBuffer error by removing emoji character

--- api/index.ts
+++ api/index.ts
@@ -82,7 +82,7 @@ db.connect()
 
 // Routes
 app.get('/', (_req: Request, res: Response) => {
-  res.json({ message: 'EduExamine API is running 🎓' });
+  res.json({ message: 'EduExamine API is running' });
 });
 
 app.use('/api/protected', protectedRoutes);
