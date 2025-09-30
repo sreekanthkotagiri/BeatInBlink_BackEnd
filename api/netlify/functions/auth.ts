7e0961ef32696d4f07b8b94e56e2e49b55e4c566
sreekanthkotagiri <sreekotagiri4u@gmail.com>
2025-09-30 21:45:01 +0530

    Updated auth.ts

--- api/netlify/functions/auth.ts
+++ api/netlify/functions/auth.ts
@@ -1,5 +1,4 @@
 import serverless from "serverless-http";
-import app from "../../src/app.js";
+import app from "../../src/app";
 
-// Netlify needs "handler"
 export const handler = serverless(app);
